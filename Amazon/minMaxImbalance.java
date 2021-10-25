3a7bf20a75931252472bb88b6555f3edc969eb7f
nemanjarajic <nrajic30@gmail.com>
2021-10-24 17:46:29 -0700

    Amazon update

--- Amazon/minMaxImbalance.java
+++ Amazon/minMaxImbalance.java
@@ -3,48 +3,71 @@ package Amazon;
 
 import java.util.*;
 
-public class minMaxImbalance {
-    private static final int MOD = 1_000_000_007;
-    public static int solution(int [] A){
-        int n = A.length;
-		
-        /* monotonic stack storing indices; for two indice i < j in stack: A[i] <= A[j];
-           Namely, every index is "waiting" for an incoming index k from the right s.t. A[j] > A[k] 
-        */
-        Deque<Integer> stack = new LinkedList<>();
-        stack.offerLast(-1);
-      
-        long res = 0;
-        
-        for (int i = 0; i < n; i++) {
-            while (stack.peekLast() != -1 && A[stack.peekLast()] > A[i]) {
-                /* 
-                For current index i, denote the top index of stack as idx, the element before idx
-                as prev. Then if we have A[idx] > A[i], we know for A[idx],
-                    1. A[i] is the next element s.t. A[i] < A[idx]
-                    2. A[prev] is the prev element s.t. A[prev] <= A[idx]
-                    
-                Then A[idx] is the minimum element for all subarrays with 
-                    1. starting point range from [prev + 1, idx]  ==> (idx - prev) choices 
-                    2. ending point range from [idx, i - 1]       ==> (i - idx) choices
-                */          
-                int idx = stack.pollLast();
-                res = (res + (long)A[idx] * (i - idx) * (idx - stack.peekLast())) % MOD;
+/*
+        int ans = 0;
+        stack<int> stack;
+        for (int i = 0, j; i <= A.size(); ++i) {
+            while (!stack.empty() && A[stack.top()] > (i == A.size() ? 0 : A[i])) {
+                j = stack.top(), stack.pop();
+                int k = stack.empty() ? -1 : stack.top();
+                ans = (ans + A[j] * (i - j) * (j - k)) % int(1e9 + 7);
             }
-            stack.offerLast(i);
+            stack.push(i);
         }
-        
-        while (stack.peekLast() != -1) {
-            int idx = stack.pollLast();
-            res = (res + (long)A[idx] * (n - idx) * (idx - stack.peekLast())) % MOD;
-        }
-        
-        return (int)res;
+        return ans;
+
+
+*/
+
+public class minMaxImbalance {
+    public static int mod = 1000000007;
+    public static int minDiff(int [] arr){
+        int sum = 0;
+	    int currentStackValue = 0;
+
+	// int[]{value, count}
+	    Deque<int[]> stack = new ArrayDeque<>();
+	    for (int i = 0; i < arr.length; i++) {
+		    int currentCount = 1;
+		    int currentValue = arr[i];
+		
+		    while (!stack.isEmpty() && stack.peek()[0] >= currentValue) {
+			    int[] popped = stack.pop();
+			    currentStackValue -= popped[1] * popped[0];
+			    currentCount += popped[1]; // assign all previous count to current
+		    }
+		    stack.push(new int[]{currentValue, currentCount});
+		    currentStackValue += currentValue * currentCount;
+		    sum = (sum + currentStackValue) % mod;
+	    }
+	    return sum;
+    }
+
+    public static int maxDiff(int [] arr){
+        int sum = 0;
+	    int currentStackValue = 0;
+
+	// int[]{value, count}
+	    Deque<int[]> stack = new ArrayDeque<>();
+	    for (int i = 0; i < arr.length; i++) {
+		    int currentCount = 1;
+		    int currentValue = arr[i];
+		
+		    while (!stack.isEmpty() && stack.peek()[0] <= currentValue) {
+			    int[] popped = stack.pop();
+			    currentStackValue -= popped[1] * popped[0];
+			    currentCount += popped[1]; // assign all previous count to current
+		    }
+		    stack.push(new int[]{currentValue, currentCount});
+		    currentStackValue += currentValue * currentCount;
+		    sum = (sum + currentStackValue) % mod;
+	    }
+	    return sum;
     }
 
     public static void main(String[] args) {
         int [] A = {1,3,2};
 
-        System.out.println(solution(A));
+        System.out.println(maxDiff(A)-minDiff(A));
     }
 }
